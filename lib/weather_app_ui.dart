e8938d2dfcf176cad4d8457c708c070cf8f787f9
Cynthia Peter <cynthiapeterpearl@gmail.com>
2020-04-18 01:47:07 +0100

    UI in progress

--- lib/weather_app_ui.dart
+++ lib/weather_app_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WeatherApp extends StatefulWidget {
   @override
@@ -9,9 +10,9 @@ class _WeatherAppState extends State<WeatherApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown[900],
-      body:  Center(
-        child:Column(
+      backgroundColor: Colors.brown[800],
+      body: Center(
+        child: Column(
           children: <Widget>[
             Container(
               alignment: Alignment.topRight,
@@ -19,11 +20,30 @@ class _WeatherAppState extends State<WeatherApp> {
               child: Icon(Icons.sort, color: Colors.white, size: 35),
             ),
             Container(
-              height:50,
+              height: 30,
               color: Colors.red,
+              //child: ,
             ),
+            SizedBox(height: 60),
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 0, 280.0, 0.0),
+              child: Text(
+                'Awka',
+              style: GoogleFonts.lato(fontSize: 40, color: Colors.white,
+              ),
+            ),),
+            Container(margin: EdgeInsets.fromLTRB(10, 5.0, 130.0, 0.0),
+              child:
+              Text('12:48 AM - Saturday, 18th March 2020', style: GoogleFonts.raleway(color: Colors.white),)
+            ,),
+            SizedBox(height: 60),
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 5.0, 290.0, 0.0),
+              child: Text('19', style: GoogleFonts.raleway(color: Colors.white, fontSize: 80),
+            ),)
           ],
-        ),),
+        ),
+      ),
     );
   }
 }
